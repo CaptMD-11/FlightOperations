b00af50246c2a987574ffcc21408666577f0b1a6
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2023-01-13 15:00:33 -0800

    another commit - still good

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
 
-        DistanceCalculator obj = new DistanceCalculator("KLAX", "YSSY");
+        DistanceCalculator obj = new DistanceCalculator("KLAX", "KSAN");
 
         System.out.println(obj.getDistance());
     }
