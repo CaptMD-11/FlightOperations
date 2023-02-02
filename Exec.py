5b886c6ca919c07c76612999642f4582f94cf11d
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2023-02-02 11:53:09 -0800

    added some file writing code

--- Exec.py
+++ Exec.py
@@ -3,7 +3,23 @@ from AirportDistances import AirportDistances
 
 fr_api = FlightRadar24API()
 
+str_addon = "test"
 
-airline_icao = "UPS"
-thy_flights = fr_api.get_flights(airline=airline_icao)
-print(thy_flights)
+for i in range(5):
+    file = open(str_addon + ".txt", "x")
+    file = open(str_addon + ".txt", "a")
+    file.write("hello")
+    file.close()
+    str_addon += "1"
+
+# airline_icao = "UPS"
+# thy_flights = fr_api.get_flights(airline=airline_icao)
+# print(thy_flights)
+
+
+# class FlightOperations:
+
+#     def write_to_file():
+#         file = open("fr_api.txt", "x")
+#         file = open("fr_api.txt", "a")
+#         file.write("as;ldfjl;asdkfj")
